0b2a0a2090cf26e0fd9ee843b50223ffb6f3c3f6
christopher webb <chris.webb5249@gmail.com>
2023-02-18 05:20:03 -0500

    add in flask files

---
+++ browserview/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def home():
+    """Homepage"""
+    return render_template('index.html', title='Home')
