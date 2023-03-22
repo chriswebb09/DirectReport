56abdcc167d2f8dd66b34f2a5326cf0755c479de
christopher webb <chris.webb5249@gmail.com>
2023-03-22 07:06:23 -0400

    fix

--- DirectReport/__init__.py
+++ DirectReport/__init__.py
@@ -8,7 +8,7 @@ file = Path(__file__).resolve()
 package_root_directory = file.parents[1]
 sys.path.append(str(package_root_directory))
 
-from .browserview import app
-from .commandline import commandline, entry, entry_storage, storage, weekly_storage
+from browserview import app
+from commandline import commandline, entry, entry_storage, storage, weekly_storage
 
 __version__ = "0.1.0"
