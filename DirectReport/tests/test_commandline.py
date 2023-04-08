aa3350e4b4e9f49e115a0b736f9f25c9aaf04906
christopher webb <chris.webb5249@gmail.com>
2023-04-07 23:17:32 -0400

    fix linting error

--- DirectReport/tests/test_commandline.py
+++ DirectReport/tests/test_commandline.py
@@ -6,12 +6,14 @@ from DirectReport.commandline.commandline import launch
 
 runner = CliRunner()
 
+
 def test_prompt_new():
-   response = runner.invoke(new, input='wau wau\n')
-   assert response.exit_code == 0
-   assert "What have you been working on" in response.output
+    response = runner.invoke(new, input='wau wau\n')
+    assert response.exit_code == 0
+    assert "What have you been working on" in response.output
+
 
 def test_prompt_launch():
-   response = runner.invoke(launch)
-   assert response.exit_code == 0
-   assert "Serving Flask app 'DirectReport.browserview.app'" in response.output
+    response = runner.invoke(launch)
+    assert response.exit_code == 0
+    assert "Serving Flask app 'DirectReport.browserview.app'" in response.output
