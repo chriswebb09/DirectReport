433b58274a4819263fab2f8131cc68e1b89e681d
Christopher Webb <13937686+chriswebb09@users.noreply.github.com>
2023-04-16 15:56:37 -0400

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -42,7 +42,7 @@ git config --global user.name "chriswebb09"
 git config --global user.email "chriswebb09@users.noreply.github.com"
 
 docroot=`mktemp -d`
-rsync -av "build/html/" "${docroot}/"
+rsync -av "build/html/" "${docroot}"
 
 pushd "${docroot}"
 
