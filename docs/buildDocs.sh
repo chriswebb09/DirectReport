3254c507764d3995ee7f789bccb3578f74a96de6
Christopher Webb <13937686+chriswebb09@users.noreply.github.com>
2023-04-16 16:04:52 -0400

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -74,6 +74,7 @@ git add .
 
 # commit all the new files
 msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF} by ${GITHUB_ACTOR}"
+
 git commit -am "${msg}"
 
 # overwrite the contents of the gh-pages branch on our github.com repo
