af35db380940a3bc1e7daa3a5b1a7617b89b80ce
Christopher Webb <13937686+chriswebb09@users.noreply.github.com>
2023-04-16 19:33:55 -0400

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -39,11 +39,8 @@ make -C docs html
 # Update GitHub Pages #
 #######################
 
-git config --global user.name "chriswebb09"
-git config --global user.email "chriswebb09@users.noreply.github.com"
-
 docroot=`mktemp -d`
-rsync -av "docs/_build/html/" "${docroot}/"
+rsync -av "docs/build/html/" "${docroot}/"
 pushd $docroot
  
 # don't bother maintaining history; just generate fresh
