e37baea41bb4c34abe9944c5f01bca62e195b898
chriswebb09 <chris.webb5249@gmail.com>
2023-05-03 14:17:37 -0400

    test

--- DirectReport/database/note_storage.py
+++ DirectReport/database/note_storage.py
@@ -4,8 +4,8 @@ import sqlite3
 import uuid
 from DirectReport.models.note import Note
 
-class NoteDataStore:
 
+class NoteDataStore:
     def __init__(self, db_path, conn=None):
         """
         Initializes the NotesDataStore object with the given SQLite database file path.
