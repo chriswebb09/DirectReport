28c7018f1340a4235d22603465fcb8f6361c5b34
chriswebb09 <chris.webb5249@gmail.com>
2023-05-03 10:43:37 -0400

    fix

--- DirectReport/__init__.py
+++ DirectReport/__init__.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
-from . import models
-from . import database
-from . import tests
-from . import commandline
-from . import browserview
 
 __version__ = "0.1.0"
--- DirectReport/database/__init__.py
+++ DirectReport/database/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-from . import blockers_storage
-from . import entry_storage
-
-
 """
 Init file for database classes
 """
