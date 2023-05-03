0654b93e308cfab3b5666d3df397184fb07f6ad3
chriswebb09 <chris.webb5249@gmail.com>
2023-05-03 08:55:26 -0400

    fix

--- DirectReport/database/notes_storage.py
+++ DirectReport/database/notes_storage.py
@@ -1,6 +1,12 @@
 import sqlite3
 import uuid
+from pathlib import Path
+import sys
 from DirectReport.models import note
+
+file = Path(__file__).resolve()
+package_root_directory = file.parents[1]
+sys.path.append(str(package_root_directory))
 
 
 class NotesDataStore:
--- DirectReport/models/list_builder.py
+++ DirectReport/models/list_builder.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-
 import datetime
 import sys
 import uuid
