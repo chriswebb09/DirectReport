f4f885121559657830e1b1f1a2ae708b4f700735
chriswebb09 <chris.webb5249@gmail.com>
2023-09-15 11:20:00 -0400

    updated total ux flow for openai pieces

--- DirectReport/browserview/app.py
+++ DirectReport/browserview/app.py
@@ -2,12 +2,9 @@
 
 # Flask
 
-from DirectReport.models.list_builder import ListBuilder
-from DirectReport.models.weekly_builder import WeeklyBuilder
-from DirectReport.database.entry_storage import EntryStorage
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, json
 
-# AI Models
+# OpenAI 
 
 import openai
 import secrets
@@ -37,9 +34,25 @@ def page_not_found(e):
 
 
 @app.route("/teamreport", methods=['GET', 'POST'])
-def teamreport():
-    report = get_team_summarys_from_git_shortlog("Adrian Prantl (67):\n add mangling testcase\n Debug Info: Represent private discriminators in DWARF.\n Revert \"Debug Info: Represent private discriminators in DWARF.\"\n Debug Info: Represent private discriminators in DWARF.\n Un-XFAIL and update test.\n Move the logic for ignoring the debug locations for closure setup code into SILGen. NFC-ish.\n Debug Info: Associate a function call with the beginning of the expression.\n Debug Info / SILGen: fix the source location of variable assignments\n typo\n Fix the debug locations of inserted operations in AvailableValueAggregator.\n Don't emit shadow copies for anonymous variables.\n Remove dead API IRGenDebugInfo::setArtificialTrapLocation().\n Use compiler-generated location for func.-sig.-spec. thunks\n whitespace\n Fix the missing inlined-at field of function-level SILDebugScopes.\n Add debug info support for inlined and specialized generic variables.\n Revert \"Add debug info support for inlined and specialized generic variables.\"\n Add debug info support for inlined and specialized generic variables.\n Update mangling prefix in Mangling.rst\n Add initial support for debug info for coroutine allocas.\n Temporarily disable failing test case, rdar://problem/43340064\n Add build-script support for the Swift LLDB backwards-compatibility tests.\n Remove accidentally committed debugging code\n Deserialize Swift compatibility version in CompilerInvocation::loadFromSerializedAST()\n SILGen: Preserve function argument debug info for arguments needing alloc_stack\n Use as the filename for SILLocation-less functions to avoid misleading source locatio\nns in backtraces.\n Add a -verify-linetable LLVM option.\n Enable debug info for inlined generics by default. It works now.\n Fix nonasserts compilation\n\nAhmad Alhashemi (5):\n [Parser] Detect nonbreaking space U+00A0 and fixit\n Move non-breaking space handling to lexUnknown\n Add more non-breaking space test cases\n Minor style edits\n Add tests for non-breaking space detect and fix-it\n\nAkshay Shrimali (1):\n Update README.md\n\nAlan Zeino (1):\n Fix typo in code example in libSyntax README\n\nAlbin \"albinek\" Sadowski (1):\n Fix syntax highlighting in CHANGELOG (#15107)\n\nAlejandro (3):\n Remove a warning, some doc fixes (#16863)\n [SR-8178] Fix BinaryFloatingPoint.random(in:) open range returning upperBound (#17794)\n [Docs] Fix minor code typo in SILPro..Man..md\n\nAlex Blewitt (5):\n [SR-7032] Fix compare for lhs and rhs\n [SR-7036] Use || instead of && for kind comparison\n [SR-7041] Remove duplicate conditional check\n Remove duplicate verb\n [SR-7043] Remove duplicate if statement")
-    return render_template('teamreport.html', title='Team Report', data={report})
+def team_report():
+    if request.method == "POST":
+        print("POST")
+    return render_template('teamreport.html', title='Team Report', data=[])
+
+
+@app.route("/report", methods=['GET', 'POST'])
+def report():
+    prompt = ""
+    if request.method == "POST":
+        print("POST")
+        prompt = request.get_json()["prompt"]
+    report = get_team_summarys_from_git_shortlog(prompt);
+    elements = report.choices[0].message.content
+    elements = elements.replace("'", '"')
+    elements = elements.replace('"albinek"', '')
+    json_object = json.loads(elements)
+    return json_object, 201
+
 
 def new():
     """
@@ -48,54 +61,6 @@ def new():
     :return: Rendered HTML template for the list page.
     """
     return render_template('list.html', title='New Entry', data=[])
-
-
-@app.route("/list", methods=['GET', 'POST'])
-def list_entries():
-    """
-    Retrieves and renders the list of all entries.
-
-    :return: Rendered HTML template for the list page.
-    """
-    if request.method == "POST":
-        json_data = request.get_json()
-        ListBuilder.new(json_data["entry"], json_data["topic"])
-    week_id = WeeklyBuilder.get_weekly_id()
-    week = WeeklyBuilder.list_week(week_id)
-    print(week)
-    return render_template('list.html', title='List', data=week)
-
-
-@app.route('/entry/<uid>', methods=['GET', 'POST'])
-def detail(uid=None):
-    """
-    Retrieves and renders the details of a specific entry.
-
-    :param uid: The ID of the entry to display.
-    :return: Rendered HTML template for the entry details page.
-    """
-    item = EntryStorage('SQLite_Python.db')
-    if request.method == "POST":
-        json_data = request.get_json()
-        ListBuilder.update(
-            json_data["id"], json_data['entry'], json_data['topic'], json_data['created_at'], json_data['week_id']
-        )
-    entry = item.get_entry(uid).to_dict()
-    return render_template('detail.html', title='Detail', data=entry)
-
-
-@app.route('/delete/<id>', methods=['GET'])
-def delete(uid=None):
-    """
-    Deletes a specific entry and redirects to the list page.
-
-    :param uid: The ID of the entry to delete.
-    :return: Redirects to the '/list' route.
-    """
-    item = EntryStorage('SQLite_Python.db')
-    item.delete_entry(uid)
-    return redirect("/list", code=302)
-
 
 def get_team_summarys_from_git_shortlog(data):
     prompt =  "can you provide a short summary of what the team as a whole accomplished this week as well as an individual breakdown based on the following list of team members and work using the following" + "Format: \n" + "{ \n" + "'team'" + ": [{" + "\n 'name'" + ": '', " + "\n 'accomplishments'" + ": '' " + " ," + "\n 'commits'" + ": '' \n" + "}]," + "\n'report'" + ": {" + "\n 'summary'" + ": " + "\n  'highlights'" + ": [{" + "\n   'title'" + ": '' ," + "\n   'description'" + ": '' "+ "\n }], \n" + " 'conclusion'" + ": ''" + "\n}" + "\n}" + "\n" + "Data:" + data
@@ -107,7 +72,7 @@ def get_team_summarys_from_git_shortlog(data):
         max_tokens=1000,
         frequency_penalty=0.0
     )
-    return response.choices[0].message.content
+    return response
 
 
 if __name__ == "__main__":
