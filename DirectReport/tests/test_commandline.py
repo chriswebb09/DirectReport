016fcbca2a9dda96ff4aee4468a26c0e7802ed06
chriswebb09 <chris.webb5249@gmail.com>
2024-01-02 01:50:40 -0500

    update commandline

--- DirectReport/tests/test_commandline.py
+++ DirectReport/tests/test_commandline.py
@@ -6,7 +6,14 @@ from click.testing import CliRunner
 import tempfile
 import pytest
 import os
+import sys
+from pathlib import Path
 
+file = Path(__file__).resolve()
+package_root_directory = file.parents[1]
+sys.path.append(str(package_root_directory))
+
+sys.path.append('.')
 
 runner = CliRunner()
 
