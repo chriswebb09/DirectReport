a240fd4759e7f94ae32f2b9bd74bff3633f89989
chriswebb09 <chris.webb5249@gmail.com>
2024-01-06 00:45:51 -0500

    update

--- DirectReport/browserview/static/js/teamreport/teamreport_components.js
+++ DirectReport/browserview/static/js/teamreport/teamreport_components.js
@@ -130,7 +130,7 @@ const GithubEntry = (commit) => {
     )
 }
 
-const GithubButtonElement = (repos, openRepoPopover, state, repoSelected) => {
+const GithubButtonElement = (repos, openRepoPopover, state) => {
     return (
         <div className="self-center mb-1 mt-1">
             <div className="mx-0 min-w-full flex flex-col items-center">
@@ -140,7 +140,7 @@ const GithubButtonElement = (repos, openRepoPopover, state, repoSelected) => {
                             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
                         </svg>
                         <span className="px-4 py-2 tracking-wide">
-                            {repoSelected ? 'Generate Report' : 'Repository'}
+                            {state.repoSelected ? 'Generate Report' : 'Repository'}
                         </span>
                     </button>
                 )}
@@ -216,7 +216,7 @@ const spinnerUI = () => {
     )
 }
 
-const EditSummaryElem = (commits, repos, state, repoSelected, openRepoPopover) => {
+const EditSummaryElem = (commits, repos, state, openRepoPopover) => {
     return (
         <div className="lg:col-span-1 sm:col-span-3 justify-center" id="edit_summary_div">
             <div className="pb-6 pt-2 bg-blue-600 rounded-3xl px-6 shadow-[1.0px_1.0px_2.0px_1.0px_rgba(0,0,0,0.58)]">
@@ -225,7 +225,7 @@ const EditSummaryElem = (commits, repos, state, repoSelected, openRepoPopover) =
                     Github Data
                 </h1>
                 {GithubEntryElement(commits)}
-                {GithubButtonElement(repos, openRepoPopover, state, repoSelected)}
+                {GithubButtonElement(repos, openRepoPopover, state)}
             </div>
         </div>
     )
