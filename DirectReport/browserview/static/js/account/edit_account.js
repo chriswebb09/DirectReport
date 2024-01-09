0ce43c4beedd6d25ef26731f3198190b7fb220ca
chriswebb09 <chris.webb5249@gmail.com>
2024-01-09 05:21:48 -0500

    add edit profile functionality

--- DirectReport/browserview/static/js/account/edit_account.js
+++
@@ -1,4 +0,0 @@
-const { useState, useEffect } = React;
-
-const domContainer = document.querySelector('#root');
-ReactDOM.render(<UserAccount action="/edit" header="Edit Account" button_title="Update Account"/>, domContainer);
